3f99b9816178e90995ffcfae63449bba280843e1
Maduki-tech <d.schlueter1011@gmail.com>
2024-01-20 21:50:34 +0100

    add delete to linked list

--- src/main/java/de/schlueter/datastructures/LinkedList.java
+++ src/main/java/de/schlueter/datastructures/LinkedList.java
@@ -2,6 +2,7 @@ package de.schlueter.datastructures;
 
 public class LinkedList {
     Node head;
+    Node tail;
 
     static class Node {
         Node node;
@@ -9,33 +10,105 @@ public class LinkedList {
 
         Node(int value) {
             this.value = value;
+            this.node = null;
         }
     }
 
     LinkedList() {
+        this.head = null;
+        this.tail = null;
     }
 
+    /**
+     * Add a node to the head of the list
+     *
+     * @param value 
+     * @return 
+     */
     public Node addNode(int value) {
         Node temp = new Node(value);
         if (head == null) {
-            head = new Node(value);
+            head = temp;
+            tail = temp;
         } else {
-            Node p = head;
-            while (p.node != null) {
-                p = p.node;
+            while (tail.node != null) {
+                tail = tail.node;
             }
 
-            p.node = temp;
+            tail.node = temp;
         }
         return head;
     }
 
+    /**
+     * Print the full linked list
+     */
     public void print() {
         Node p = head;
-        while(p != null){
+        while (p != null) {
             System.out.println(p.value);
             p = p.node;
         }
+    }
+
+    /**
+     * Get the node by its **index**
+     *
+     * @param index 
+     * @return 
+     */
+    public int getNode(int index) {
+        Node result = head;
+        int i = 0;
+        while(result != null){
+            if(i == index){
+                return result.value;
+            }
+
+            result = result.node;
+            i++;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Get the Length of the Linked List
+     *
+     * @return Length of the linked list
+     */
+    public int length() {
+        int result = 0;
+        Node p = head;
+
+        while (p != null) {
+            result++;
+            p = p.node;
+        }
+
+        return result;
+    }
+
+    /**
+     * Remove Value by its index and return it
+     *
+     * @param index 
+     * @return 
+     */
+    public int removeByIndex(int index){
+        int value = 0;
+        int i = 0;
+        Node p = head;
+        while (p != null){
+            if(i == index){
+                p.node = p.node.node;
+                value = p.value;
+                break;
+            }
+            p = p.node;
+            i++;
+        }
+        return value;
     }
 
 }
--- src/test/java/de/schlueter/datastructures/LinkedList_Test.java
+++ src/test/java/de/schlueter/datastructures/LinkedList_Test.java
@@ -1,11 +1,15 @@
 package de.schlueter.datastructures;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class LinkedList_Test {
+
     @Test
-    void test(){
+    void printLength_Test() {
         LinkedList sut = new LinkedList();
+
         sut.addNode(1);
         sut.addNode(3);
         sut.addNode(4);
@@ -13,9 +17,28 @@ public class LinkedList_Test {
         sut.addNode(6);
         sut.addNode(0);
 
-        sut.print();
-
+        assertEquals(6, sut.length());
     }
 
-    
+    @Test
+    void delete_Test(){
+        LinkedList sut = new LinkedList();
+
+        sut.addNode(1);
+        sut.addNode(3);
+        sut.addNode(4);
+        sut.addNode(8);
+        sut.addNode(6);
+        sut.addNode(0);
+
+        int res = sut.removeByIndex(2);
+
+        assertEquals(4, res);
+        assertEquals(5, sut.length());
+
+        System.out.println(sut.length());
+
+
+
+    }
 }
